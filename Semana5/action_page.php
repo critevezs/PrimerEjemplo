d8473eaa252b2dc49362bda0a71d991d8fad3daa
critevezs <54085883+critevezs@users.noreply.github.com>
2019-09-10 22:13:13 -0300

    Add files via upload

---
+++ Semana5/action_page.php
@@ -0,0 +1,37 @@
+<?php
+ $MiObjeto = new stdClass();
+ $MiObjeto->Nombre =$_GET ["Nombre"];
+ $MiObjeto->Apellido =$_GET ["Apellido"];
+
+ $archivo=fopen('usuarios.txt', "a");
+ fwrite($archivo,json_encode($MiObjeto)."\n");
+ fclose($archivo);
+
+
+ 
+
+
+
+
+ 	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
