5960737eddb70f1be0a70cc45cce0750fa0657a4
critevezs <54085883+critevezs@users.noreply.github.com>
2019-09-17 22:13:42 -0300

    Add files via upload

---
+++ semana6/listado.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<h2> Listado de Usuarios</h2>
+
+<ol>
+	<?php
+$miArchivo = fopen("usuarios.txt", "r");
+while(!feof($miArchivo)) {
+  $objeto =json_decode(fgets($miArchivo));
+  echo "<li>".$objeto->Nombre."</li>";
+}
+fclose($miArchivo);
+
+?>
+  
+</ol> 
+
+</body>
+</html>
+
+
